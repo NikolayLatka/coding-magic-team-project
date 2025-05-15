227df960c1f12431c88695a891b34bb78f2dd04f
NikitosekPro <148691930+NikitosekPro@users.noreply.github.com>
2025-05-15 20:30:29 +0200

    rps fix
    I added sum add-ons on rps card. I think this looks nice!

--- src/JS/rock-paper-scissors.js
+++ src/JS/rock-paper-scissors.js
@@ -1,4 +1,3 @@
-
 const rockBox = document.querySelector(".rock-box");
 const paperBox = document.querySelector(".paper-box");
 const scissorsBox = document.querySelector(".scissors-box");
@@ -8,21 +7,18 @@ const computerButton = document.querySelector(".computer-result");
 
 const userScoreEl = document.getElementById("user-score");
 const computerScoreEl = document.getElementById("computer-score");
-
+const computerChoiceText = document.getElementById("computer-choice-text");
 
 let userScore = 0;
 let computerScore = 0;
 let computerChoice = null;
 
-
 const choices = ["rock", "paper", "scissors"];
-
 
 function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * choices.length);
 return choices[randomIndex];
 }
-
 
 function decideWinner(user, computer) {
 if (user === computer) return "draw";
@@ -38,7 +34,6 @@ if (
 }
 }
 
-
 function updateResult(winner) {
 if (winner === "user") {
     userScore++;
@@ -52,27 +47,54 @@ if (winner === "user") {
     resultMessage.textContent = "Нічия!";
     resultMessage.style.color = "gray";
 }
+
 userScoreEl.textContent = userScore;
 computerScoreEl.textContent = computerScore;
 }
-
 
 computerButton.addEventListener("click", () => {
 if (!computerChoice) {
     alert("Спочатку зробіть свій вибір!");
     return;
 }
+
 alert(`Комп’ютер обрав: ${computerChoice}`);
 });
 
 function playRound(userChoice) {
 computerChoice = getComputerChoice();
+computerChoiceText.textContent = computerChoice;
 
 const winner = decideWinner(userChoice, computerChoice);
 updateResult(winner);
 }
 
-
 rockBox.addEventListener("click", () => playRound("rock"));
 paperBox.addEventListener("click", () => playRound("paper"));
 scissorsBox.addEventListener("click", () => playRound("scissors"));
+
+
+function clearAllShadows() {
+    rockBox.style.boxShadow = "none";
+    paperBox.style.boxShadow = "none";
+    scissorsBox.style.boxShadow = "none";
+}
+
+function changeRockColor() {
+    clearAllShadows();
+    rockBox.style.boxShadow = "0px 0px 50px grey";
+}
+
+function changePaperColor() {
+    clearAllShadows();
+    paperBox.style.boxShadow = "0px 0px 50px grey";
+}
+
+function changeScissorsColor() {
+    clearAllShadows();
+    scissorsBox.style.boxShadow = "0px 0px 50px grey";
+}
+
+rockBox.addEventListener("click", changeRockColor);
+paperBox.addEventListener("click", changePaperColor);
+scissorsBox.addEventListener("click", changeScissorsColor);
