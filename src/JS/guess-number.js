f3f52d2a07201113ba5c72edf8b21924977fc866
1234351s <156699518+1234351s@users.noreply.github.com>
2025-05-19 19:10:14 +0300

    Додав також у ви програли скобки

--- src/JS/guess-number.js
+++ src/JS/guess-number.js
@@ -9,7 +9,7 @@ document.querySelector(".search-icon").addEventListener("click", function () {
             resultMessage.textContent = `Вітаю, ви вгадали число!(${randomNumber})`;
             resultMessage.style.color = "rgb(3, 153, 0)";
         } else {
-            resultMessage.textContent = `Ви програли, Комп'ютер згадав ${randomNumber}`;
+            resultMessage.textContent = `Ви програли, Комп'ютер згадав (${randomNumber})`;
             resultMessage.style.color = "red";
         }
     }
